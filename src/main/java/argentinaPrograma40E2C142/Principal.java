ec8e96d9c2de7750afe6f956ffa9373fd893b64a
Mariano Ruiz <144689905+MarianoEJRuiz@users.noreply.github.com.>
2023-11-17 21:06:29 -0300

    inicial

---
+++ src/main/java/argentinaPrograma40E2C142/Principal.java
@@ -0,0 +1,10 @@
+package argentinaPrograma40E2C142;
+
+import modelos.Alumno;
+
+public class Principal {
+    public static void main (String[]args){
+        Alumno alumno = new Alumno();
+        System.out.println(alumno.getNombre());
+    }
+}
---
+++ src/main/java/modelos/Alumno.java
@@ -0,0 +1,9 @@
+package modelos;
+
+import lombok.Data;
+
+@Data
+public class Alumno {
+    public String nombre;
+    public String apellido;
+}
